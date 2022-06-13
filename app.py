3ec69fc86daaccebfa05aa35770cfcd2d3e2ac5e
Murilo Trevejo <murilotrevejo09@gmail.com>
2022-06-13 11:29:05 -0300

    updated

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from flask import json, request, render_template
 
 app = flask.Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def home():
-    #json testing
+    # json testing
     data_set = {'Page': 'Home', 'Message': 'Welcome to api calculator homepage!'}
     json_dump = json.dumps(data_set)
 
     return json_dump
 
+
 @app.route("/operate", methods=["GET"])
 def operate():
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
